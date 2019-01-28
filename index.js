1ecff9adf5306a7752c0ae760fa5e02819f8817b
Jorge <jorge.arancibia.jeal@gmail.com>
2019-01-28 05:15:27 -0300

    Calculadora lista

--- index.js
+++ index.js
@@ -8,22 +8,20 @@ const BTN7 = document.getElementById('in7');
 const BTN8 = document.getElementById('in8');
 const BTN9 = document.getElementById('in9');
 const BTN0 = document.getElementById('in0');
-var btnMas = document.getElementById('inMas');
-var btnMenos = document.getElementById('inMenos');
-var btnPor = document.getElementById('inPor');
-var btnDiv = document.getElementById('inDiv');
-var btnPorcent = document.getElementById('inPorcent');
-var btnPot = document.getElementById('XX');
-var btnResult = document.getElementById('inResult');
-var btnCE = document.getElementById('CE');
-var borrarTodo = document.getElementById('C');
+const BTNMAS = document.getElementById('inMas');
+const BTNMENOS = document.getElementById('inMenos');
+const BTNPOR = document.getElementById('inPor');
+const BTNDIV = document.getElementById('inDiv');
+const BTNPORCENT = document.getElementById('inPorcent');
+const BTNPOT = document.getElementById('inN');
+const BTNRESULT = document.getElementById('inResult');
+const BTNCE = document.getElementById('inCE');
+const BTNC = document.getElementById('inC');
 const BTNPUNTO = document.getElementById('inPunto');
 var lb = document.getElementById('pantalla');
+var add1, add2, operacion, porcentaje;
 
-// btnPor.addEventListener('click', function(e){
-// 	var resultado =  * ;
-// 	lb.innerHTML = resultado;
-// });
+// NÚMEROS
 
 BTN1.addEventListener('click', function(e){
 	lb.innerHTML = lb.innerHTML + 1;
@@ -59,7 +57,97 @@ BTN0.addEventListener('click', function(e){
 	lb.innerHTML = lb.innerHTML + 0;
 });
 
-// function limpiar(){
-// 	lb.innerHTML = "";
-// }
+// ACCIONES
 
+BTNC.addEventListener('click', function(e){
+	reset();
+});
+BTNCE.addEventListener('click', function(e){
+	limpiar();
+});
+
+BTNMAS.addEventListener('click', function(e){
+	add1 = lb.innerHTML;
+	operacion = '+';
+	lb.innerHTML = "";
+});
+BTNMENOS.addEventListener('click', function(e){
+	add1 = lb.innerHTML;
+	operacion = '-';
+	lb.innerHTML = "";
+});
+BTNPOR.addEventListener('click', function(e){
+	add1 = lb.innerHTML;
+	operacion = 'x';
+	lb.innerHTML = "";
+});
+BTNDIV.addEventListener('click', function(e){
+	add1 = lb.innerHTML;
+	operacion = '÷';
+	lb.innerHTML = "";
+});
+BTNPORCENT.addEventListener('click', function(e){
+	add1 = lb.innerHTML;
+	operacion = '%';
+	lb.innerHTML = "";
+});
+BTNPOT.addEventListener('click', function(e){
+	add1 = lb.innerHTML;
+	operacion = 'n';
+	lb.innerHTML = "";
+});
+BTNRESULT.addEventListener('click', function(e){
+	add2 = lb.innerHTML;
+	resultado();
+});
+
+// Botón para restar el IVA de un número.
+// BTNPORCENT.addEventListener('click', function(e){
+// 	const IVA = 0.19;
+// 	resultado = lb.innerHTML * IVA;
+// 	var nIVA = lb.innerHTML - resultado;
+// 	lb.innerHTML = nIVA;
+// });
+// Botón para restar el IVA de un número.
+// BTNPORCENT.addEventListener('click', function(e){
+// 	const IVA = 0.19;
+// 	resultado = lb.innerHTML * IVA;
+// 	var nIVA = lb.innerHTML + resultado;
+// 	lb.innerHTML = nIVA;
+// });
+
+
+function limpiar(){
+	lb.innerHTML = "";
+}
+function reset(){
+	lb.innerHTML = "";
+	add1 = 0;
+	add2 = 0;
+	operacion = "";
+}
+function resultado() {
+	var res = 0;
+	switch(operacion){
+		case '+':
+			res = parseFloat(add1) + parseFloat(add2);
+			break;
+		case '-':
+			res = parseFloat(add1) - parseFloat(add2);
+			break;
+		case 'x':
+			res = parseFloat(add1) * parseFloat(add2);
+			break;
+		case '÷':
+			res = parseFloat(add1) / parseFloat(add2);
+			break;
+		case '%':
+			res = (parseFloat(add1) / 100) * parseFloat(add2);
+			break;
+		case 'n':
+			res = parseFloat(add1) ** parseFloat(add2);
+			break;
+	}
+	reset();
+	lb.innerHTML = res;
+}
